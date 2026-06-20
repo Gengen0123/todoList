f4c4f873c28c07c8b0a6bc833cb28a13945ed8da
sota <soutanarisawa200795nari@gmail.com>
2026-06-20 10:40:28 +0900

    Todo追加機能を実装

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 // TODO: この処理はJavaScriptの読み込み確認用です。
 // Issue #2「Todo追加機能」の実装時に削除します。
 console.log("script.js が読み込まれました");
+
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+todoForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // ページのリロード防止
+
+    const text = todoInput.value;
+
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    todoList.appendChild(li);
+
+    todoInput.value = "";
+});
